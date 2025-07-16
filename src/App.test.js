dff7e1e03def68e78b7b1568d6ae00aa2e8a3937
SUMEG-04 <sumeg04112001@gmail.com>
2025-07-16 15:24:26 +0530

    updated test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
-import Login from './Login';
+import Login from './Components/Login.js';
 import { UserContext } from '../App';
 import { MemoryRouter } from 'react-router-dom';
 
