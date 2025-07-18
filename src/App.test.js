473dd23311956346918f03d829de70ee58e0f77f
SUMEG-04 <sumeg04112001@gmail.com>
2025-07-18 13:46:06 +0530

    added correct path of login for test

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
-import Login from './Components/Login.js';
+import Login from './Components/Login.jsx';
 import { UserContext } from '../App';
 import { MemoryRouter } from 'react-router-dom';
 
